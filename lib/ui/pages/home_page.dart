5e0e62a7ceaef791145c19790e766dd4b2b9f4b2
Wahyu <wahyufajar65@gmail.com>
2023-12-15 15:47:50 +0700

    add top profile in home

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -51,6 +51,57 @@ const HomePage({ Key? key }) : super(key: key);
         child: Image.asset('assets/ic_plus_circle.png',width: 24,),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal:24),
+        children: [
+          buildProfile()
+        ],
+      ),
     );
   }
+}
+
+Widget buildProfile(){
+  return Container(
+    margin: const EdgeInsets.only(top:40),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Fajar,', style: greyTextStyle.copyWith(fontSize: 16),),
+            const SizedBox(height: 2,),
+            Text('Ananda',style:blackTextStyle.copyWith(fontSize: 20,fontWeight: semiBold))
+          ],
+        ),
+        Container(
+          width: 60,
+          height: 60,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(image: AssetImage('assets/img_profile.png',))
+          ),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              width: 16,
+              height: 16,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: whiteColor
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.check_circle,
+                  color: greenColor,
+                  size: 14,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }

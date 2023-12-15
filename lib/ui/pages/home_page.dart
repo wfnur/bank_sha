a84ec5dfd16e8d52963337d257ed2275f48b2a30
Wahyu <wahyufajar65@gmail.com>
2023-12-15 16:02:37 +0700

    add wallet card in home

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -54,54 +54,87 @@ const HomePage({ Key? key }) : super(key: key);
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal:24),
         children: [
-          buildProfile()
+          buildProfile(),
+          buildWallet()
         ],
       ),
     );
   }
-}
 
-Widget buildProfile(){
-  return Container(
-    margin: const EdgeInsets.only(top:40),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Fajar,', style: greyTextStyle.copyWith(fontSize: 16),),
-            const SizedBox(height: 2,),
-            Text('Ananda',style:blackTextStyle.copyWith(fontSize: 20,fontWeight: semiBold))
-          ],
-        ),
-        Container(
-          width: 60,
-          height: 60,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(image: AssetImage('assets/img_profile.png',))
+  Widget buildProfile(){
+    return Container(
+      margin: const EdgeInsets.only(top:40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Fajar,', style: greyTextStyle.copyWith(fontSize: 16),),
+              const SizedBox(height: 2,),
+              Text('Ananda',style:blackTextStyle.copyWith(fontSize: 20,fontWeight: semiBold))
+            ],
           ),
-          child: Align(
-            alignment: Alignment.topRight,
-            child: Container(
-              width: 16,
-              height: 16,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: whiteColor
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.check_circle,
-                  color: greenColor,
-                  size: 14,
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(image: AssetImage('assets/img_profile.png',))
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
                 ),
               ),
             ),
-          ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildWallet(){
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(top:30),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit:BoxFit.cover,
+          image:AssetImage('assets/img_bg_card.png')
         )
-      ],
-    ),
-  );
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Ananda Fajar', style: whiteTextStyle.copyWith(fontSize: 18,fontWeight: medium),),
+          const SizedBox(height: 25,),
+
+          Text('**** **** **** 1234', style: whiteTextStyle.copyWith(fontSize: 18,fontWeight: medium,letterSpacing: 6),),
+          const SizedBox(height: 21,),
+
+          Text('Balance', style: whiteTextStyle,),
+          Text('Rp. 1.0000.0000.0000', style: whiteTextStyle.copyWith(fontSize: 20,fontWeight: semiBold),),
+
+
+        ],
+      ),
+    );
+  }
+
 }

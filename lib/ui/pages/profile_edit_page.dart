2d44672f3aebe2facfd18e0d3eabcf5b4fc33ee2
Wahyu <wahyufajar65@gmail.com>
2023-12-29 08:02:19 +0700

    add profile edit

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,6 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,6 +8,53 @@ const ProfileEditPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Edit Profile'
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children:  [
+          const SizedBox(height: 40,),
+          //box
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Username
+                const CustomFormTextField(title: 'Username'),
+                const SizedBox(height: 16,),
+                //Full Name
+                const CustomFormTextField(title: 'Full Name'),
+                const SizedBox(height: 16,),
+                //Email
+                const CustomFormTextField(title: 'Email Address'),
+                const SizedBox(height: 16,),
+                //PASSWORD
+                const CustomFormTextField(title: 'Password',obscureText: true,),
+                const SizedBox(height: 16,),
+                //Submit
+                const SizedBox(height: 16,),
+                //Button Sign in
+                CustomFilledButton(
+                      title: "Update",
+                      onPressed: () {
+                        
+                      },
+                ),
+              ]
+            ),
+          ),
+          const SizedBox(height: 30,),
+        ],
+      ),
+    );
   }
 }

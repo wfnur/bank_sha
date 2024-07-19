9c7cbd2bdda0889b42c2fa087037702d94c7f4cf
wahyU <wahyu@ptgdi.com>
2024-07-19 16:20:55 +0700

    home : add wallet card

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -51,6 +51,81 @@ class HomePage extends StatelessWidget {
         child: Image.asset('assets/ic_plus_circle.png',width: 24,),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          buildProfile(),
+          buildWalletCard()
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfile(){
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Howdy,',style: greyTextStyle.copyWith(fontSize: 16),),
+              const SizedBox(height: 2,),
+              Text('Jho Wijk',style: blackTextStyle.copyWith(fontSize: 20,fontWeight: semiBold),)
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(image: AssetImage('assets/img_profile.png'))
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor
+                ),
+                child: Center(
+                  child: Icon(Icons.check_circle,color: greenColor,size: 18,)
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard(){
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(top:30),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/img_bg_card.png')
+          )
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Jhon Wijk ',style: whiteTextStyle.copyWith(fontSize: 18,fontWeight: medium),),
+          const SizedBox(height: 28,),
+          Text('**** **** **** 8990 ',style: whiteTextStyle.copyWith(fontSize: 18,fontWeight: medium,letterSpacing: 6),),
+          const SizedBox(height: 21,),
+          Text('Balance',style: whiteTextStyle),
+          Text('Rp. 1.000.000',style: whiteTextStyle.copyWith(fontSize: 24,fontWeight: semiBold)),
+        ],
+      ),
     );
   }
 }

607807480f0b3d47ba7f3a7148b8dfbfb764075e
wahyU <wahyu@ptgdi.com>
2024-07-19 15:50:24 +0700

    bottom navigation bar with  FloatingActionButton

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,32 +8,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(fontSize: 10,fontWeight: medium),
-        unselectedLabelStyle: blackTextStyle.copyWith(fontSize: 10,fontWeight: medium),
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/ic_overview.png',width: 20,color: blueColor,),
-            label: 'Overview',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/ic_history.png',width: 20,),
-            label: 'History'
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/ic_statistic.png',width: 20,),
-            label: 'Statistic'
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/ic_reward.png',width: 20,),
-            label: 'Rewards'
-          ),
-        ],
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          backgroundColor: whiteColor,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(fontSize: 10,fontWeight: medium),
+          unselectedLabelStyle: blackTextStyle.copyWith(fontSize: 10,fontWeight: medium),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_overview.png',width: 20,color: blueColor,),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_history.png',width: 20,),
+              label: 'History'
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_statistic.png',width: 20,),
+              label: 'Statistic'
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/ic_reward.png',width: 20,),
+              label: 'Rewards'
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        backgroundColor: purpleColor,
+        onPressed: (){},
+        child: Image.asset('assets/ic_plus_circle.png',width: 24,),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
